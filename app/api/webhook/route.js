272e88c94005448b53daff7da84ca8c1a09c6a13
Rahul Jain <r.rahuljain23@gmail.com>
2026-01-19 16:58:18 +0530

    Update route.js

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,6 +1,5 @@
-import { notifyTelegram } from "@/lib/notify/telegram";
-import { evaluateNotificationRules } from "@/lib/notify/rules";
-
+import { notifyTelegram } from "../../../../lib/notify/telegram";
+import { evaluateNotificationRules } from "../../../../lib/notify/rules";
 import { kv } from "@vercel/kv";
 
 export const dynamic = "force-dynamic";
